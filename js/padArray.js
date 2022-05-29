2a2e220a8b31ede9c21e4aa489dbbb9c82d9070d
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-29 15:28:40 -0700

    js working

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,15 @@
 // REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
-
+exports.pad = (array, minSize, value=null) => {
+    if (array.length >= minSize){
+        // console.log(array)
+        return array;
+    }else{
+        for (let i = array.length; i < minSize; i++ ){
+            array.push(value);
+            
+        }
+        return array;
+    }
+    // console.log(array)
 }
+// exports.pad([0,1,2,3], 4,)
--- js/padArraySpec.js
+++ js/padArraySpec.js
@@ -1,1 +1,7 @@
-// Write unit tests!
+var sp = require("./padArray");
+
+
+console.log(sp.pad([0,1,2,3], 4,))
+console.log(sp.pad([0,1,2,3], 5,))
+console.log(sp.pad([0,1,2,3], 5, "Working"))
+console.log(sp.pad([0,2,2,3,6], 8, "Forgot"))
