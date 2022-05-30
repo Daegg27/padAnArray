1c8032ac051c23ce513755afcf82ae5bc56e8cd2
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-29 19:10:24 -0700

    Spec file

--- python/pad_array.py
+++ python/pad_array.py
@@ -6,7 +6,7 @@ def pad(array, min_size, value = None):
         for i in range(len(array), min_size):
             array.append(value)
     return array
-print(pad([0,1,2,3], 4,))
-print(pad([0,1,2,3], 5,))
-print(pad([0,1,2,3], 5, "Working"))
-print(pad([0,2,2,3,6], 8, "Forgot"))
+# print(pad([0,1,2,3], 4,))
+# print(pad([0,1,2,3], 5,))
+# print(pad([0,1,2,3], 5, "Working"))
+# print(pad([0,2,2,3,6], 8, "Forgot"))
--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,6 @@
-# Write unit tests!
+from pad_array import pad
+
+print(pad([0,1,2,3], 4,))
+print(pad([0,1,2,3], 5,))
+print(pad([0,1,2,3], 5, "Working"))
+print(pad([0,2,2,3,6], 8, "Forgot"))
